2377638c3fd7718398f24032d88041368b2e5b2b
lgib88 <113594571+lgib88@users.noreply.github.com>
2022-09-15 12:01:26 +0200

    Add files via upload

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgibert- <lgibert-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 09:17:06 by lgibert-          #+#    #+#             */
+/*   Updated: 2022/07/28 12:42:22 by lgibert-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	while (argv[0][i] != '\0' && argc)
+	{
+		ft_putchar(argv[0][i]);
+		++i;
+	}
+	ft_putchar('\n');
+	return (0);
+}
---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgibert- <lgibert-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 09:20:31 by lgibert-          #+#    #+#             */
+/*   Updated: 2022/07/28 09:20:53 by lgibert-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i] != '\0')
+		{
+			ft_putchar(argv[j][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
